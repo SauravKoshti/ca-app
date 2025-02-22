2abeea27b1846ab027bb259e0c3c275116b0d26c
kaushik-kboy <kaushikbhrahmbhatt@gmail.com>
2025-02-22 12:36:22 +0530

    document tab issue

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -19,7 +19,12 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::orderBy('id', 'desc')->get();
+        $login_user = Auth::user();
+        if ($login_user->role == 'user') {
+            $users = User::where('group_id', $login_user->group_id)->orderBy('id', 'desc')->get();
+        } else {
+            $users = User::orderBy('id', 'desc')->get();
+        }
         return view('admin.users.index', compact('users'));
     }
 
@@ -92,7 +97,7 @@ class UserController extends Controller
         $loggedInUserId = '1';
         $payments = Payment::where('user_id', $user->id)->get();
         // dd($user);
-        return view('admin.users.show', compact('user', 'documentDataArray', 'loggedInUserId','payments'));
+        return view('admin.users.show', compact('user', 'documentDataArray', 'loggedInUserId', 'payments'));
     }
 
     /**
@@ -108,7 +113,7 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-       // $validated = $request->validate([
+        // $validated = $request->validate([
         //     'name' => 'required|string|max:255',
         //     'firstname' => 'required|string|max:255',
         //     'lastname' => 'required|string|max:255',
