b1f530a474052ea4137068ddf97c93bb9ddb9cb0
SauravKoshti <sauravkoshti2000@gmail.com>
2025-02-25 00:23:10 +0530

    user footer timing

--- resources/views/users/layouts/footer.blade.php
+++ resources/views/users/layouts/footer.blade.php
@@ -1,71 +1,76 @@
 <footer>
     <div class="foote-link">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-5">
+        <div class="container">
             <div class="row">
-              <div class="col-md-4">
-                <h2>Quik Links</h2>
-                <ul class="foot-link-list">
-                  <li><a href="{{ route('home') }}">Home</a></li>
-                  <li><a href="{{ route('contact-us') }}">Contact Us</a></li>
-                  <li><a href="{{ route('about-us') }}">About Us</a></li>
-                </ul>
-              </div>
-              <div class="col-md-8">
-                <h2>Services</h2> 
-                <ul class="foot-link-list">
-                  <li><a href="{{ route('services')}}">Mutual Fund</a></li>
-                  <li><a href="{{ route('services')}}">Taxation</a></li>
-                  <li><a href="{{ route('services')}}">GST</a></li>
-                  <li><a href="{{ route('services')}}">Accounting</a></li>
-                  <li><a href="{{ route('services')}}">Pancards</a></li>
-                </ul>
-              </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h2>Quik Links</h2>
+                            <ul class="foot-link-list">
+                                <li><a href="{{ route('home') }}">Home</a></li>
+                                <li><a href="{{ route('contact-us') }}">Contact Us</a></li>
+                                <li><a href="{{ route('about-us') }}">About Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-8">
+                            <h2>Services</h2>
+                            <ul class="foot-link-list">
+                                <li><a href="{{ route('services')}}">Mutual Fund</a></li>
+                                <li><a href="{{ route('services')}}">Taxation</a></li>
+                                <li><a href="{{ route('services')}}">GST</a></li>
+                                <li><a href="{{ route('services')}}">Accounting</a></li>
+                                <li><a href="{{ route('services')}}">Pancards</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h2>Opening Hours</h2>
+                    <table class="table opening-hours-table">
+                        <tbody>
+                            <tr>
+                                <td>Monday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Tuesday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Wednesday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Thursday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Friday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Saturday</td>
+                                <td>09:00 AM - 07:30 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Sunday</td>
+                                <td>Close</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-4">
+                    <h2>Connect With Us</h2>
+                    <ul class="call-ad">
+                        <li><a href="#"><i class="fa fa-map-marker"></i><span> Station Rd, opp. Bhagvatsinhji High
+                                    School, Venkateshwara Nagar, Dhoraji, Gujarat 360410</span></a></li>
+                        <li><a href="tel:8200267836"><i class="fa fa-phone"></i><span>82002 67836</span></a></li>
+                        <li><a href="mailto:vbjagani403@gmail.com"><i
+                                    class="fa fa-envelope-o"></i><span>vbjagani403@gmail.com</span></a></li>
+                    </ul>
+                </div>
             </div>
-          </div>
-          <div class="col-md-3">
-            <h2>Opening Hours</h2>
-            <ul class="list-foot padd-inft">
-              <li><span> Monday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Tuesday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Wednesday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Thursday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Friday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Saturday </span>
-                <div class="value pull-right">09.00 AM - 07:30 PM</div>
-              </li>
-              <hr>
-              <li><span> Sunday </span>
-                <div class="value pull-right">Close</div>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <h2>Connect With Us</h2>
-            <ul class="call-ad">
-              <li><a href="#"><i class="fa fa-map-marker"></i><span> Station Rd, opp. Bhagvatsinhji High School, Venkateshwara Nagar, Dhoraji, Gujarat 360410</span></a></li>
-              <li><a href="tel:8200267836"><i class="fa fa-phone"></i><span>82002 67836</span></a></li>
-              <li><a href="mailto:vbjagani403@gmail.com"><i class="fa fa-envelope-o"></i><span>vbjagani403@gmail.com</span></a></li>
-            </ul>
-          </div>
         </div>
-      </div>
     </div>
-    
-  </footer>
+
+</footer>
